acf8b203ffd44b2844709b0b571a7f2e2329ce34
marek_baron <MarekBaron@users.noreply.github.com>
2012-12-18 23:42:06 +0100

    konwersja do vs2012 drobne porz?dki - usuni?cie martwego kodu

--- Mandelbrot/Form.cs
+++ Mandelbrot/Form.cs
@@ -22,7 +22,8 @@ namespace Baron.Mandelbrot
 
     internal struct PointD
     {
-        public PointD(double aX, double aY) : this()
+        public PointD(double aX, double aY)
+            : this()
         {
             this.X = aX;
             this.Y = aY;
@@ -40,7 +41,6 @@ namespace Baron.Mandelbrot
         private double _setStartY = -1;
         private double _setWidth = 3.5f;
         private bool _redrawing = false;
-        private bool _needsAnotherRedraw = false;
         private byte[] _palette = null;
         private Point _panStartPoint = Point.Empty;
 
@@ -51,7 +51,7 @@ namespace Baron.Mandelbrot
             InitializeGenerators();
             Redraw();
         }
-        
+
         private void InitializeGenerators()
         {
             IGenerator[] generators = new IGenerator[]
@@ -106,11 +106,6 @@ namespace Baron.Mandelbrot
         }
         #endregion
 
-        private void Form1_Resize(object sender, EventArgs e)
-        {          
-            Redraw();
-        }
-
         private PointD ScreenToSpace(int aScreenX, int aScreenY)
         {
             double coef = _setWidth / (double)pictureBox1.Width;
@@ -122,43 +117,17 @@ namespace Baron.Mandelbrot
             return aSetWidth * (double)pictureBox1.Height / (double)pictureBox1.Width;
         }
 
-        void Form1_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
-        {
-            var mouseInSpace = ScreenToSpace(e.X - pictureBox1.Location.X, e.Y - pictureBox1.Location.Y);
-
-            var oldWidth = _setWidth;
-            var oldHeight = SetWidthToSetHeight(oldWidth);
-
-            if (e.Delta < 0)
-                _setWidth *= 1.1f;            
-            else            
-                _setWidth *= 0.9f;
-                       
-            var setHeight = SetWidthToSetHeight(_setWidth);
-
-            _setStartX = mouseInSpace.X - ((mouseInSpace.X - _setStartX) * _setWidth / oldWidth);
-            _setStartY = mouseInSpace.Y - ((mouseInSpace.Y - _setStartY) * setHeight / oldHeight);
-
-            Redraw();
-        }
-
         private void Redraw()
         {
             if (!_redrawing)
-            {         
-                InvokeInternalRedraw();             
-            }
-            else
-            {                
-                _needsAnotherRedraw = true;
+            {
+                InternalRedraw();
             }
         }
 
-        private object _lockObject = new object();
-
         private void InternalRedraw()
         {
-            _redrawing = true;            
+            _redrawing = true;
             var bitmap = (Bitmap)pictureBox1.Image;
             if (bitmap == null || bitmap.Width != pictureBox1.Width || bitmap.Height != pictureBox1.Height)
             {
@@ -168,49 +137,42 @@ namespace Baron.Mandelbrot
 
             var genTask = Task.Factory.StartNew(() =>
             {
-                //lock (_lockObject)
-                //{
-                    Stopwatch sw = new Stopwatch();
-                    sw.Start();
-                    byte[] rgbaValues = _currentGenerator.Generate(bitmap.Width, bitmap.Height, _setStartX, _setWidth, _setStartY, SetWidthToSetHeight(_setWidth), _palette);
-                    sw.Stop();
-                    RefreshElapsedTime(sw.ElapsedMilliseconds);
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                //byte[] rgbaValues = new byte[bitmap.Height * bitmap.Width];
+                byte[] rgbaValues = _currentGenerator.Generate(bitmap.Width, bitmap.Height, _setStartX, _setWidth, _setStartY, SetWidthToSetHeight(_setWidth), _palette);
+                sw.Stop();
+                RefreshElapsedTime(sw.ElapsedMilliseconds);
 
-                    Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
-                    BitmapData bmpData =
-                        bitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.WriteOnly,
-                        bitmap.PixelFormat);
+                Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                BitmapData bmpData =
+                    bitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.WriteOnly,
+                    bitmap.PixelFormat);
 
-                    IntPtr ptr = bmpData.Scan0;
-                    System.Runtime.InteropServices.Marshal.Copy(rgbaValues, 0, ptr, rgbaValues.Length);
-                    // Unlock the bits.
-                    bitmap.UnlockBits(bmpData);
-                    pictureBox1.Invalidate();
-                    _redrawing = false;
-                    if (_needsAnotherRedraw)
-                    {
-                        _needsAnotherRedraw = false;
-                        //InvokeInternalRedraw();
-                    }
-                //}
+                IntPtr ptr = bmpData.Scan0;
+                System.Runtime.InteropServices.Marshal.Copy(rgbaValues, 0, ptr, rgbaValues.Length);
+                // Unlock the bits.
+                bitmap.UnlockBits(bmpData);
+                pictureBox1.Invalidate();
+                _redrawing = false;
             });
 
         }
 
         #region InvokeRedraw
-        private delegate void InvokeInternalRedrawDelegate();
+        //private delegate void InvokeInternalRedrawDelegate();
 
-        private void InvokeInternalRedraw()
-        {
-            if (this.InvokeRequired)
-            {
-                this.Invoke(new InvokeInternalRedrawDelegate(InvokeInternalRedraw));
-            }
-            else
-            {
-                InternalRedraw();
-            }
-        }  
+        //private void InvokeInternalRedraw()
+        //{
+        //    if (this.InvokeRequired)
+        //    {
+        //        this.Invoke(new InvokeInternalRedrawDelegate(InvokeInternalRedraw));
+        //    }
+        //    else
+        //    {
+        //        InternalRedraw();
+        //    }
+        //}  
         #endregion
 
         #region WriteText
@@ -247,10 +209,11 @@ namespace Baron.Mandelbrot
         }
         #endregion
 
+        #region form events
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.Button == System.Windows.Forms.MouseButtons.Left)
-            {   
+            {
                 var start = ScreenToSpace(_panStartPoint.X, _panStartPoint.Y);
                 var end = ScreenToSpace(e.Location.X, e.Location.Y);
                 _setStartX = _setStartX - (end.X - start.X);
@@ -278,6 +241,32 @@ namespace Baron.Mandelbrot
         {
             pictureBox1.Focus();
         }
+
+        void Form1_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            var mouseInSpace = ScreenToSpace(e.X - pictureBox1.Location.X, e.Y - pictureBox1.Location.Y);
+
+            var oldWidth = _setWidth;
+            var oldHeight = SetWidthToSetHeight(oldWidth);
+
+            if (e.Delta < 0)
+                _setWidth *= 1.1f;
+            else
+                _setWidth *= 0.9f;
+
+            var setHeight = SetWidthToSetHeight(_setWidth);
+
+            _setStartX = mouseInSpace.X - ((mouseInSpace.X - _setStartX) * _setWidth / oldWidth);
+            _setStartY = mouseInSpace.Y - ((mouseInSpace.Y - _setStartY) * setHeight / oldHeight);
+
+            Redraw();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Redraw();
+        }
+        #endregion
 
     }
 }
