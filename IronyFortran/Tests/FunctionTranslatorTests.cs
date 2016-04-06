3fb176c6cd6e0223109e8544a31ed4f46cacb59f
marek_baron <marek_baron@localhost>
2016-04-06 23:54:19 +0200

    Wst?pne parsowanie metody

---
+++ IronyFortran/Tests/FunctionTranslatorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace IronyFortran.Tests
+{
+    [TestFixture]
+    public class FunctionTranslatorTests
+    {
+        [TestCase("test1")]
+        [TestCase("test2")]
+        public void TestTranslation(string aFilename)
+        {
+            var path = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\Tests\TestData"));
+            var inputFilename = Path.Combine(path, aFilename) + ".input.txt";            
+            var expectedFilename = Path.Combine(path, aFilename) + ".expected.cs";
+            var outputFilename = Path.Combine(path, aFilename) + ".output.cs";
+            FileAssert.Exists(inputFilename);
+            FileAssert.Exists(expectedFilename);
+
+            var trns = new FunctionTranslator();
+            string errors;
+            var output = trns.Translate(File.ReadAllText(inputFilename), out errors);
+            File.WriteAllText(outputFilename, output);
+
+            Assert.That(errors, Is.Empty);
+            FileAssert.AreEqual(expectedFilename, outputFilename);            
+        }        
+    }
+}
---
+++ IronyFortran/Tests/TestData/test1.expected.cs
@@ -0,0 +1,4 @@
+int Tga_810(string XTGA, string[] ARRAY_810)
+{
+   string TGA;
+}
