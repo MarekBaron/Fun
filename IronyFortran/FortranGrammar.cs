f2987fcd11e4934cfdaecb43af05f8d1342b0389
marek_baron <MarekBaron@users.noreply.github.com>
2016-04-06 23:54:19 +0200

    Wst?pne parsowanie metody

---
+++ IronyFortran/FortranGrammar.cs
@@ -0,0 +1,48 @@
+﻿using Irony.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronyFortran
+{
+    [Language("VDIFortran")]
+    public class FortranGrammar : Grammar
+    {
+        public FortranGrammar() : base(caseSensitive: false)
+        {            
+            //terminals
+            var identifier = new IdentifierTerminal("identifier");
+            var number = new NumberLiteral("number");            
+
+            //nonterminals            
+            var builtinType = new NonTerminal("builtinType");
+            var stringType = new NonTerminal("string");
+            var paramList = new NonTerminal("paramList");
+            var functionHeader = new NonTerminal("functionHeader");
+            var functionFooter = new NonTerminal("functionFooter");
+            var variableDec = new NonTerminal("variableDec");
+            var variableDecList = new NonTerminal("variableDecList");
+            var variableDecListElem = new NonTerminal("variableDecList");
+            var program = new NonTerminal("program");
+
+            this.MarkPunctuation(";", ",", "(", ")", "[", "]", ":");
+            this.MarkTransient(builtinType);
+
+            //rules
+            stringType.Rule = ToTerm("CHARACTER") + "(" + number + ")";
+            builtinType.Rule = ToTerm("INTEGER") | "CHARACTER" | stringType;
+            paramList.Rule = MakeStarRule(paramList, ToTerm(","), identifier);
+            functionHeader.Rule = builtinType + ToTerm("FUNCTION") + identifier + "(" + paramList + ")" + ";";
+            functionFooter.Rule = ToTerm("END") + ToTerm("FUNCTION") + identifier +";";
+
+            variableDecListElem.Rule = identifier | identifier + "(" + number +")";
+            variableDecList.Rule = MakePlusRule(variableDecList, ToTerm(","), variableDecListElem);
+            variableDec.Rule = builtinType + variableDecList + ";";
+
+            program.Rule = functionHeader + variableDec + functionFooter;
+            this.Root = program;
+        }
+    }
+}
---
+++ IronyFortran/FunctionTranslator.cs
@@ -0,0 +1,32 @@
+﻿using Irony.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronyFortran
+{
+    public class FunctionTranslator
+    {
+        public string Translate(string anInput, out string anErrors)
+        {
+            var grammar = new FortranGrammar();
+
+            LanguageData language = new LanguageData(grammar);
+
+            Parser parser = new Parser(language);
+
+            ParseTree parseTree = parser.Parse(anInput);
+
+            anErrors = parseTree
+                .ParserMessages.Any() ? 
+                    parseTree
+                    .ParserMessages
+                    .Select(m => $"{m.Level.ToString()} {m.Location} {m.Message}")
+                    .Aggregate((a, s) => a + Environment.NewLine + s)
+                : String.Empty;
+            return string.Empty;
+        }
+    }
+}
