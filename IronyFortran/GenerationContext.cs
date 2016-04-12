29fb071821abe35b8c4d375b1a86a6835f97dd6e
marek_baron <marek_baron@localhost>
2016-04-12 21:03:40 +0200

    Pierwsze podej?cie do w?z??w AST

---
+++ IronyFortran/GenerationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronyFortran
+{
+    public class GenerationContext
+    {
+        private readonly Dictionary<string, HashSet<string>> _parameterNames = new Dictionary<string, HashSet<string>>(StringComparer.InvariantCultureIgnoreCase);
+
+        public void AddInputParameter(string aFunctionName, string aParameterName)
+        {
+            HashSet<string> hs = null;
+            if(!_parameterNames.TryGetValue(aFunctionName, out hs))
+            {
+                hs = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+                _parameterNames.Add(aFunctionName, hs);
+            }
+        }
+
+        public bool IsInputParameter(string aFunctionName, string aParameterName)
+        {
+            return _parameterNames[aFunctionName].Contains(aParameterName);
+        }
+    }
+}
---
+++ IronyFortran/GeneratorNodes/NoGenerationNode.cs
@@ -0,0 +1,25 @@
+﻿using Irony.Ast;
+using Irony.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronyFortran.GeneratorNodes
+{
+    public class NoGenerationNode : BaseNode
+    {
+        public override void Generate(int anIndent, StringBuilder aSB)
+        {
+            throw new InvalidOperationException("Wezeł nie przeznaczony do generacji");
+        }
+
+        public ParseTreeNode ParseTreeNode { get; private set; }
+
+        public override void Init(AstContext context, ParseTreeNode treeNode)
+        {
+            ParseTreeNode = treeNode;
+        }
+    }
+}
