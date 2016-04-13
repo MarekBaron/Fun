4847c325c8af883a71e290f10c90c9f051f071f2
marek_baron <marek_baron@localhost>
2016-04-13 23:00:41 +0200

    ExpressionListNode

---
+++ IronyFortran/GeneratorNodes/ExpressionListNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony.Ast;
+using Irony.Parsing;
+
+namespace IronyFortran.GeneratorNodes
+{
+    /// <summary/>    
+    public class ExpressionListNode : BaseNode
+    {
+        public override void Generate(GenerationContext aContext, int anIndent, StringBuilder aSB)
+        {
+            for (int i = 0; i < _expressions.Count - 1; i++)
+            {
+                _expressions[i].Generate(aContext, anIndent, aSB);
+                aSB.Append(", ");
+            }
+            _expressions.Last().Generate(aContext, anIndent, aSB);
+        }
+
+        protected override void InitInternal(AstContext context, ParseTreeNode parseNode)
+        {
+            _expressions = parseNode.ChildNodes
+                .Select(n => n.AstNode)
+                .Cast<ExpressionNode>()
+                .ToList();
+        }
+
+        private List<ExpressionNode> _expressions;
+        public IEnumerable<ExpressionNode> Expressions { get { return _expressions; } }
+    }
+}
