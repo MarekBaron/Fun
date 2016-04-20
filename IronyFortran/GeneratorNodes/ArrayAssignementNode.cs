2ce055b4990bda54a9b9b0458f045321f9760517
marek_baron <MarekBaron@users.noreply.github.com>
2016-04-20 23:51:39 +0200

    Przypisanie do elementu tablicy Parsowanie stringow przypisanych do zmiennych nie b?d?cych stringami Specjalna obs?uga VDICWERT przechodzi test translation_1

---
+++ IronyFortran/GeneratorNodes/ArrayAssignementNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony.Ast;
+using Irony.Parsing;
+
+namespace IronyFortran.GeneratorNodes
+{
+    public class ArrayAssignmentNode : BaseNode
+    {
+        public override void Generate(GenerationContext aContext, int anIndent, StringBuilder aSB)
+        {
+            VariableName.Generate(aContext, anIndent, aSB);
+            aSB.Append("[");
+            Index.Generate(aContext, anIndent, aSB);
+            aSB.Append("] = ");
+            Value.Generate(aContext, anIndent, aSB);
+        }
+
+        protected override void InitInternal(AstContext context, ParseTreeNode parseNode)
+        {
+            VariableName = (IdentifierValueNode)parseNode.ChildNodes[0].AstNode;
+            Index = (BaseNode)parseNode.ChildNodes[1].AstNode;
+            Value = (BaseNode)parseNode.ChildNodes[2].AstNode;
+        }
+
+        public BaseNode Value { get; private set; }
+        public BaseNode Index { get; private set; }
+        public IdentifierValueNode VariableName { get; private set; }
+    }
+}
